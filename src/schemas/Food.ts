4db0eea6226e586495ef683bb5e0e71ab1c061a4
JUNGHO-GIT <107819345+JUNGHO-GIT@users.noreply.github.com>
2023-08-25 08:48:51 +0900

    Update Food.ts

--- src/schemas/Food.ts
+++ src/schemas/Food.ts
@@ -1,7 +1,7 @@
 // Food.ts
 import mongoose from "mongoose";
 
-const FoodScheme = new mongoose.Schema({
+const FoodScheme = new mongoose.Schema ({
   _id: mongoose.Schema.Types.ObjectId,
   user_id: { type: String, required: true },
   food_name: { type: String, required: true },
@@ -12,8 +12,8 @@ const FoodScheme = new mongoose.Schema({
   food_protein: { type: Number, required: true },
   food_fat: { type: Number, required: true },
   food_calories_goal: { type: Number, required: false },
-  food_regdate: { type: Date, default: Date.now, required: true },
-  food_update: { type: Date, default: Date.now, required: true }
+  food_regdate: { type: String, default: () => new Date().toISOString().split('T')[0], required: true },
+  food_update: { type: String, default: () => new Date().toISOString().split('T')[0], required: true }
 });
 
 export default mongoose.model("Food", FoodScheme);
