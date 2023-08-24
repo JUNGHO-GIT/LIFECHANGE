7730b1d874584a9e6bd8c792602b79007f13f3cf
JUNGHO-GIT <junghomun00@gmail.com>
2023-08-24 14:00:19 +0900

    modified:   client/package-lock.json modified:   client/package.json modified:   client/src/page/food/FoodDetail.tsx modified:   client/src/page/food/FoodList.tsx

--- client/src/page/food/FoodDetail.tsx
+++ client/src/page/food/FoodDetail.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom';
-import axios from "axios";
-import { useParams } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 
 // ------------------------------------------------------------------------------------------------>
@@ -28,22 +26,121 @@ const FoodDetailStyle = createGlobalStyle`
 const FoodDetail = () => {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
+  const [showPerGram, setShowPerGram] = useState(0);
+
+  const title:any = params.get("title");
+  const brand:any = params.get("brand");
+
+  const calories:any = Number(params.get("calories"));
+  const fat:any = Number(params.get("fat"));
+  const carb:any = Number(params.get("carb"));
+  const protein:any = Number(params.get("protein"));
+  const serving:any = params.get("serving");
+
+  // ---------------------------------------------------------------------------------------------->
+  const totalGram = () => {
+    return (
+      <>
+      <h5 className="card-title">
+        {title ? title : "x"}
+      </h5>
+      <p className="card-text">
+        <span>브랜드 : </span>
+        {brand ? brand : "x"}
+      </p>
+      <p className="card-text">
+        <span>칼로리 : </span>
+        {calories ? calories : 0}
+      </p>
+      <p className="card-text">
+        <span>지방 : </span>
+        {fat ? fat : 0}
+      </p>
+      <p className="card-text">
+        <span>탄수화물 : </span>
+        {carb ? carb : 0}
+      </p>
+      <p className="card-text">
+        <span>단백질 : </span>
+        {protein ? protein : 0}
+      </p>
+      <p className="card-text">
+        <span>용량 : </span>
+        {serving ? serving : 0}
+      </p>
+      </>
+    );
+  };
+
+  // ---------------------------------------------------------------------------------------------->
+  const oneGram = () => {
+    const servingValue = serving.toString();
+    const regexRules = /(\d+)g/gm;
+    const match = servingValue.match(regexRules);
+
+    if (match) {
+      const regexValue = match[0].replace("g", "");
+      return Number(regexValue);
+    }
+  };
+
+  // ---------------------------------------------------------------------------------------------->
+  const perGram = (params: any) => {
+
+    const oneGramValue:any = oneGram();
+    const paramsValue = Number(params);
+
+    const caloriesPer:any = ((calories/oneGramValue)*(paramsValue)).toFixed(1);
+    const fatPer:any = ((fat/oneGramValue)*(paramsValue)).toFixed(1);
+    const carbPer:any = ((carb/oneGramValue)*(paramsValue)).toFixed(1);
+    const proteinPer:any = ((protein/oneGramValue)*(paramsValue)).toFixed(1);
+
+    return (
+      <>
+      <p className="card-text">
+        <span>칼로리 : </span>
+        {caloriesPer ? caloriesPer : "x"}
+      </p>
+      <p className="card-text">
+        <span>지방 : </span>
+        {fatPer ? fatPer : "x"}
+      </p>
+      <p className="card-text">
+        <span>탄수화물 : </span>
+        {carbPer ? carbPer : "x"}
+      </p>
+      <p className="card-text">
+        <span>단백질 : </span>
+        {proteinPer ? proteinPer : "x"}
+      </p>
+      </>
+    );
+  };
 
   // ---------------------------------------------------------------------------------------------->
   return (
-    <div className="container">
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{params.get("title")}</h5>
-          <p className="card-text">{params.get("brand")}</p>
-          <p className="card-text">{params.get("calories")} 칼로리</p>
-          <p className="card-text">{params.get("food")} 지방</p>
-          <p className="card-text">{params.get("carb")} 탄수화물</p>
-          <p className="card-text">{params.get("protein")} 단백질</p>
-          <p className="card-text">{params.get("serving")}</p>
+    <>
+      <FoodDetailStyle />
+      <div className="empty-h100"></div>
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            {totalGram()}
+          </div>
+        </div>
+        <div className="empty-h20"></div>
+        <div className="input-group">
+          <input type="number" className="form-control" placeholder="그램 수 입력" onChange={e => setShowPerGram(Number(e.target.value))} />
+        </div>
+        <div className="empty-h20"></div>
+        <div className="card">
+          <div className="card-body">
+            {showPerGram > 0 && perGram(showPerGram)}
+          </div>
         </div>
       </div>
-    </div>
+      <div className="empty-h100"></div>
+    </>
   );
 };
 
--- client/src/page/food/FoodList.tsx
+++ client/src/page/food/FoodList.tsx
@@ -10,80 +10,123 @@ const FoodList = () => {
   const lang = "ko";
 
   const fetchData = () => {
-    const url
-    = `https://fat-git-main-jungho-git.vercel.app/api/${lang}/search?query=${query}&page=${page}`;
-    axios
-    .get(url)
+    const url = `https://fat-git-main-jungho-git.vercel.app/api/${lang}/search?query=${query}&page=${page}`;
+    axios.get(url)
     .then((response) => {
       setItems(response.data.items);
-      console.log(response.data.items);
     })
     .catch((error) => {
       console.error(error);
     });
   };
 
-  const handleSearchChange = (e:any) => {
+  // ---------------------------------------------------------------------------------------------->
+  const handleSearchChange = (e: any) => {
     setQuery(e.target.value);
   };
 
-  const handleNextPage = () => {
-    setPage((prevPage) => prevPage + 1);
-    fetchData();
+  const handleSearchButton = (e: any) => {
+    e.preventDefault();
+    if (!query) {
+      alert("검색어를 입력하세요");
+      return false;
+    }
+    else {
+      fetchData();
+    }
   };
 
-  const handlePrevPage = () => {
-    setPage((prevPage) => Math.max(prevPage - 1, 1));
-    fetchData();
+  const handleNextPage = (e: any) => {
+    e.preventDefault();
+    if (!query) {
+      alert("검색어를 입력하세요");
+      return false;
+    }
+    else {
+      setPage((prevPage) => prevPage + 1);
+      fetchData();
+    }
+  };
+
+  const handlePrevPage = (e: any) => {
+    e.preventDefault();
+    if (!query) {
+      alert("검색어를 입력하세요");
+      return false;
+    }
+    else {
+      setPage((prevPage) => Math.max(prevPage - 1, 1));
+      fetchData();
+    }
   };
 
   // ---------------------------------------------------------------------------------------------->
   return (
+    <>
+    <br />
+    <br />
     <div className="container">
-      <div className="input-group mb-3">
-        <input
-          type="text"
-          className="form-control"
-          value={query}
-          onChange={handleSearchChange}
-          placeholder="검색"
-        />
-        <div className="input-group-append">
-          <button className="btn btn-primary" onClick={fetchData}>
-            검색
-          </button>
-        </div>
-      </div>
       <div className="row">
-        {items.map((item:any, index) => (
-          <div key={index} className="col-md-4">
-            <div className="card mb-4">
-              <div className="card-body">
-                <h5 className="card-title">
-                  <Link to={`/foodDetail?title=${item.title}&brand=${item.brand}&calories=${item.calories}&food=${item.food}&carb=${item.carb}&protein=${item.protein}&serving=${item.serving}`}>
-                    {item.title}
-                  </Link>
-                </h5>
-                <p className="card-text">{item.brand}</p>
-                <p className="card-text">{item.calories} 칼로리</p>
-                <p className="card-text">{item.food} 지방</p>
-                <p className="card-text">{item.carb} 탄수화물</p>
-                <p className="card-text">{item.protein} 단백질</p>
-                <p className="card-text">{item.serving}</p>
-              </div>
+        <div className="col-12">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              value={query}
+              onChange={handleSearchChange}
+            />
+            <div className="input-group-append">
+              <button className="btn btn-primary" onClick={handleSearchButton}>검색</button>
             </div>
           </div>
-        ))}
-      </div>
-      <div className="btn-group mt-3">
-        <button className="btn btn-primary" onClick={handlePrevPage}>
-          이전
-        </button>
-        <button className="btn btn-primary" onClick={handleNextPage}>
-          다음
-        </button>
+          <div className="btn-group mt-3">
+            <button className="btn btn-primary ms-2" onClick={handlePrevPage}>이전</button>
+            <button className="btn btn-primary ms-2" onClick={handleNextPage}>다음</button>
+          </div>
+          <br/>
+          <div className="row">
+            {items.map((item: any, index) => (
+              <div key={index} className="col-md-4">
+                <div className="card mb-4">
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      <Link to={`/foodDetail?title=${item.title}&brand=${item.brand}&calories=${item.calories}&food=${item.food}&carb=${item.carb}&protein=${item.protein}&serving=${item.serving}`}>
+                        {item.title}
+                      </Link>
+                    </h5>
+                    <p className="card-text">
+                      <span>브랜드 : </span>
+                      {item.brand ? item.brand : "x"}
+                    </p>
+                    <p className="card-text">
+                      <span>칼로리 : </span>
+                      {item.calories ? item.calories : 0}
+                    </p>
+                    <p className="card-text">
+                      <span>지방 : </span>
+                      {item.food ? item.food : 0}
+                    </p>
+                    <p className="card-text">
+                      <span>탄수화물 : </span>
+                      {item.carb ? item.carb : 0}
+                    </p>
+                    <p className="card-text">
+                      <span>단백질 : </span>
+                      {item.protein ? item.protein : 0}
+                    </p>
+                    <p className="card-text">
+                      <span>용량 : </span>
+                      {item.serving ? item.serving : 0}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
+    </>
   );
 };
 
