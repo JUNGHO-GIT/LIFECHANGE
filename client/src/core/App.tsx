cc8897be298dc226c4f666bf04b5bed2da7d512b
JUNGHO-GIT <107819345+JUNGHO-GIT@users.noreply.github.com>
2023-08-30 09:00:02 +0900

    Update App.tsx

--- client/src/core/App.tsx
+++ client/src/core/App.tsx
@@ -1,6 +1,6 @@
 // App.tsx
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import "../assets/Custom.css";
@@ -12,40 +12,40 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "react-datepicker/dist/react-datepicker.css";
 import "../assets/DatePicker.css";
 
-import {Resize} from "../components/Resize";
-import {Loader} from "../components/Loader";
-import {Header} from "../layout/Header";
-import {Footer} from "../layout/Footer";
-import {Hover} from "../components/Hover";
-import {Main} from "../page/common/Main";
+import { Resize } from "../components/Resize";
+import { Loader } from "../components/Loader";
+import { Header } from "../layout/Header";
+import { Footer } from "../layout/Footer";
+import { Hover } from "../components/Hover";
+import { Main } from "../page/common/Main";
 
-import {UserLogin} from "../page/user/UserLogin";
-import {UserInsert} from "../page/user/UserInsert";
-import {UserDetail} from "../page/user/UserDetail";
-import {UserUpdate} from "../page/user/UserUpdate";
-import {UserDelete} from "../page/user/UserDelete";
-import {UserList} from "../page/user/UserList";
+import { UserLogin } from "../page/user/UserLogin";
+import { UserInsert } from "../page/user/UserInsert";
+import { UserDetail } from "../page/user/UserDetail";
+import { UserUpdate } from "../page/user/UserUpdate";
+import { UserDelete } from "../page/user/UserDelete";
+import { UserList } from "../page/user/UserList";
 
-import {BoardList} from "../page/board/BoardList";
-import {BoardInsert} from "../page/board/BoardInsert";
-import {BoardDetail} from "../page/board/BoardDetail";
-import {BoardUpdate} from "../page/board/BoardUpdate";
+import { BoardList } from "../page/board/BoardList";
+import { BoardInsert } from "../page/board/BoardInsert";
+import { BoardDetail } from "../page/board/BoardDetail";
+import { BoardUpdate } from "../page/board/BoardUpdate";
 
-import {CalendarList} from "../page/calendar/CalendarList";
-import {CalendarDetail} from "../page/calendar/CalendarDetail";
+import { CalendarList } from "../page/calendar/CalendarList";
+import { CalendarDetail } from "../page/calendar/CalendarDetail";
 
-import {FoodList} from "../page/food/FoodList";
-import {FoodInsert} from "../page/food/FoodInsert";
-import {FoodTotal} from "../page/food/FoodTotal";
-import {FoodDetail} from "../page/food/FoodDetail";
-import {FoodInfo} from "../page/food/FoodInfo";
+import { FoodList } from "../page/food/FoodList";
+import { FoodInsert } from "../page/food/FoodInsert";
+import { FoodTotal } from "../page/food/FoodTotal";
+import { FoodDetail } from "../page/food/FoodDetail";
+import { FoodInfo } from "../page/food/FoodInfo";
 
-import {WorkoutList} from "../page/workout/WorkoutList";
-import {WorkoutInsert} from "../page/workout/WorkoutInsert";
+import { WorkoutList } from "../page/workout/WorkoutList";
+import { WorkoutInsert } from "../page/workout/WorkoutInsert";
+import { WorkoutDetail } from "../page/workout/WorkoutDetail";
 
 // ------------------------------------------------------------------------------------------------>
 const App = () => {
-
   // ---------------------------------------------------------------------------------------------->
   return (
     <div className="App">
@@ -75,6 +75,7 @@ const App = () => {
           <Route path="/foodInfo" element={<FoodInfo />} />
           <Route path="/workoutList" element={<WorkoutList />} />
           <Route path="/workoutInsert" element={<WorkoutInsert />} />
+          <Route path="/workoutDetail" element={<WorkoutDetail />} />
         </Routes>
         <Footer />
       </Router>
