509112a58ab3230ffc2d4b70a7236d4f57f3b9ad
JUNGHO-GIT <107819345+JUNGHO-GIT@users.noreply.github.com>
2023-09-05 08:39:33 +0900

    Update FoodTotal.tsx

--- client/src/page/food/FoodTotal.tsx
+++ client/src/page/food/FoodTotal.tsx
@@ -1,22 +1,23 @@
-// FoodTotal.tsx
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+// FoodSearch.tsx
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import axios from "axios";
 
 // ------------------------------------------------------------------------------------------------>
-export const FoodTotal = () => {
-  const [FOOD_TOTAL, setFOOD_TOTAL] = useState([]);
+export const FoodSearch = () => {
 
+  const [FOOD_SEARCH, setFOOD_SEARCH] = useState([]);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(0);
+  const lang = "ko";
+  const user_id = sessionStorage.getItem("user_id");
   const koreanDate = new Date();
   koreanDate.setHours(koreanDate.getHours() + 9);
-  const [food_regdate, setFood_regdate] = useState(
-    koreanDate.toISOString().split("T")[0]
-  );
-
-  const user_id = useLocation().state.user_id;
-  const URL_FOOD = process.env.REACT_APP_URL_FOOD;
-  const TITLE = "Food Total";
+  const food_regdate = koreanDate.toISOString().split("T")[0];
+  const navParam = useNavigate();
+  const URL_FOOD_API = process.env.REACT_APP_URL_FOOD_API;
+  const TITLE = "Food Search";
 
   // ---------------------------------------------------------------------------------------------->
   const datePicker = () => {
@@ -27,50 +28,53 @@ export const FoodTotal = () => {
         popperPlacement="bottom"
         onChange={(date: any) => {
           const selectedDate = date.toISOString().split("T")[0];
-          setFood_regdate(selectedDate);
         }}
+        readOnly
       />
     );
   };
 
   // ---------------------------------------------------------------------------------------------->
-  useEffect(() => {
-    const fetchFoodTotal = async () => {
-      try {
-        const response = await axios.get(`${URL_FOOD}/foodTotal`, {
-          params: {
-            user_id: user_id,
-            food_regdate: food_regdate,
-          },
-        });
-        setFOOD_TOTAL(response.data);
-      } catch (error: any) {
-        alert(`Error fetching food data: ${error.message}`);
-        setFOOD_TOTAL([]);
-      }
-    };
-    fetchFoodTotal();
-  }, [user_id, food_regdate]);
+  const fetchFoodSearch = () => {
+    const url = `${URL_FOOD_API}/${lang}/search?query=${query}&page=${page}`;
+
+    axios.get(url)
+    .then((response) => {
+      setFOOD_SEARCH(response.data.items);
+    })
+    .catch((error: any) => {
+      alert(`Error fetching food data: ${error.message}`);
+      setFOOD_SEARCH([]);
+    });
+  };
 
   // ---------------------------------------------------------------------------------------------->
-  const foodTotalTable = () => {
+  const foodSearchTable = () => {
     return (
-      <table className="table table-striped">
+      <table className="table table-striped table-bordered">
         <thead>
           <tr>
+            <th>이름</th>
+            <th>브랜드</th>
             <th>칼로리</th>
+            <th>지방</th>
             <th>탄수화물</th>
             <th>단백질</th>
-            <th>지방</th>
+            <th>용량</th>
           </tr>
         </thead>
         <tbody>
-          {FOOD_TOTAL.map((index: any) => (
+          {FOOD_SEARCH.map((index : any) => (
             <tr>
-              <td>{index.food_calories}</td>
-              <td>{index.food_carb}</td>
-              <td>{index.food_protein}</td>
-              <td>{index.food_fat}</td>
+              <td>
+                <Link to={`/foodInsert?title=${index.title}&brand=${index.brand}&calories=${index.calories}&fat=${index.fat}&carb=${index.carb}&protein=${index.protein}&serving=${index.serving}`}>{index.title}</Link>
+              </td>
+              <td>{index.brand ? index.brand : "x"}</td>
+              <td>{index.calories ? index.calories : 0}</td>
+              <td>{index.fat ? index.fat : 0}</td>
+              <td>{index.carb ? index.carb : 0}</td>
+              <td>{index.protein ? index.protein : 0}</td>
+              <td>{index.serving ? index.serving : 0}</td>
             </tr>
           ))}
         </tbody>
@@ -79,32 +83,131 @@ export const FoodTotal = () => {
   };
 
   // ---------------------------------------------------------------------------------------------->
+  const handleSearchChange = (e: any) => {
+    e.preventDefault();
+    setQuery(e.target.value);
+  };
+  const handleSearchButton = (e: any) => {
+    e.preventDefault();
+    if (!query) {
+      alert("검색어를 입력하세요");
+      return false;
+    }
+    else {
+      fetchFoodSearch();
+    }
+  };
+  const handleNextPage = (e: any) => {
+    e.preventDefault();
+    if (!query) {
+      alert("검색어를 입력하세요");
+      return false;
+    }
+    else {
+      setPage((prevPage) => prevPage + 1);
+      fetchFoodSearch();
+    }
+  };
+  const handlePrevPage = (e: any) => {
+    e.preventDefault();
+    if (!query) {
+      alert("검색어를 입력하세요");
+      return false;
+    }
+    else {
+      setPage((prevPage) => Math.max(prevPage - 1, 1));
+      fetchFoodSearch();
+    }
+  };
+  const buttonFoodTotal = () => {
+    navParam(`/foodTotal`, {
+      state: {
+        user_id : user_id,
+        food_regdate : food_regdate
+      }
+    });
+  };
+  const buttonFoodMorning = () => {
+    navParam(`/foodDetail`, {
+      state: {
+        user_id : user_id,
+        food_regdate : food_regdate,
+        food_category : "morning"
+      }
+    });
+  };
+  const buttonFoodLunch = () => {
+    navParam(`/foodDetail`, {
+      state: {
+        user_id : user_id,
+        food_regdate : food_regdate,
+        food_category : "lunch"
+      }
+    });
+  };
+  const buttonFoodDinner = () => {
+    navParam(`/foodDetail`, {
+      state: {
+        user_id : user_id,
+        food_regdate : food_regdate,
+        food_category : "dinner"
+      }
+    });
+  };
+  const buttonFoodSnack = () => {
+    navParam(`/foodDetail`, {
+      state: {
+        user_id : user_id,
+        food_regdate : food_regdate,
+        food_category : "snack"
+      }
+    });
+  };
+
+  // ---------------------------------------------------------------------------------------------->
   return (
     <div className="container">
       <div className="row d-center mt-5">
         <div className="col-12">
-          <h1 className="mb-3 fw-9">
-            {TITLE}
-            <span className="ms-4">(Total)</span>
-          </h1>
+          <h1 className="mb-3 fw-9">{TITLE}</h1>
         </div>
       </div>
       <div className="row d-center mt-5">
-        <div className="col-12">
+        <div className="col-10">
           <h1 className="mb-3 fw-5">
             <span className="ms-4">{datePicker()}</span>
           </h1>
         </div>
       </div>
       <div className="row d-center mt-5">
-        <div className="col-8">
-          {foodTotalTable()}
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
+        <div className="col-12">
+          {foodSearchTable()}
+        </div>
+      </div>
+      <div className="row d-center mt-5">
+        <div className="col-4">
+          <div className="btn-group mb-3">
+            <button className="btn btn-primary ms-2" onClick={handlePrevPage}>이전</button>
+            <button className="btn btn-primary ms-2" onClick={handleNextPage}>다음</button>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" value={query}
+            onChange={handleSearchChange}/>
+            <button className="btn btn-primary" onClick={handleSearchButton}>검색</button>
+          </div>
+        </div>
+      </div>
+      <div className="row d-center mt-5">
+        <div className="col-10">
+          <div className="btn-group">
+            <button className="btn btn-primary ms-2" onClick={buttonFoodTotal}>총합</button>
+            <button className="btn btn-primary ms-2" onClick={buttonFoodMorning}>아침</button>
+            <button className="btn btn-primary ms-2" onClick={buttonFoodLunch}>점심</button>
+            <button className="btn btn-primary ms-2" onClick={buttonFoodDinner}>저녁</button>
+            <button className="btn btn-primary ms-2" onClick={buttonFoodSnack}>간식</button>
+          </div>
         </div>
       </div>
     </div>
