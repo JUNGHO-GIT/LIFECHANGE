5b8eca15ad74e46d41e083d50464944c3def9806
JUNGHO-GIT <junghomun00@gmail.com>
2023-09-08 20:56:10 +0900

    modified:   client/src/layout/Header.tsx

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -2,170 +2,95 @@ import React from "react";
 import {Link} from "react-router-dom";
 import "../assets/Custom.css";
 
+// -------------------------------------------------------------------------------------------->
 export const Header = () => {
+
   const user_id = window.sessionStorage.getItem("user_id");
 
-  // ---------------------------------------------------------------------------------------------->
-  const NavList = () => {
+  // -------------------------------------------------------------------------------------------->
+  const DropdownItem:React.FC<{to: string, label: string }> = ({ to, label }) => (
+    <li className="py-1">
+      <Link to={to} className="dropdown-item">
+        {label}
+      </Link>
+    </li>
+  );
 
-    // -------------------------------------------------------------------------------------------->
+  // -------------------------------------------------------------------------------------------->
+  const DropdownMenu:React.FC<{ label: string, items: {to: string, label: string }[] }> = ({ label, items }) => (
+    <li className="me-5">
+      <div className="dropdown ms-2">
+        <a className="text-decoration-none text-light dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          {label}
+        </a>
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          {items.map(item => (
+            <DropdownItem key={item.to} {...item} />
+          ))}
+        </ul>
+      </div>
+    </li>
+  );
+
+  // -------------------------------------------------------------------------------------------->
+  const NavList = () => {
+    const menus = [
+      {
+        label: "Main",
+        items: [
+          {to: "/", label: "Home"},
+          {to: "/test", label: "Test"}
+        ]
+      },
+      {
+        label: "User",
+        items: [
+          {to: "/userList", label: "UserList"},
+          {to: "/userLogin", label: "Login"},
+          {to: "/userInsert", label: "Signup"}
+        ]
+      },
+      {
+        label: "Board",
+        items: [
+          {to: "/boardList", label: "BoardList"},
+          {to: "/boardInsert", label: "BoardInsert"}
+        ]
+      },
+      {
+        label: "Calendar",
+        items: [
+          {to: "/calendarList", label: "CalendarList"}
+        ]
+      },
+      {
+        label: "Food",
+        items: [
+          {to: "/foodSearch", label: "FoodSearch"},
+          {to: "/foodList", label: "FoodList"}
+        ]
+      },
+      {
+        label: "Workout",
+        items: [
+          {to: "/workoutList", label: "WorkoutList"},
+          {to: "/workoutInsert", label: "WorkoutInsert"}
+        ]
+      },
+      {
+        label: "Sleep",
+        items: [
+          {to: "/sleepListDay", label: "SleepDay"},
+          {to: "/sleepListWeek", label: "SleepWeek"},
+          {to: "/sleepListMonth", label: "SleepMonth"},
+          {to: "/sleepListYear", label: "SleepYear"},
+          {to: "/sleepListSelect", label: "SleepSelect"}
+        ]
+      }
+    ];
     return (
-      <ul className="nav">
-        {/* main */}
-        <li>
-          <div className="dropdown ms-2">
-            <a className="text-decoration-none text-light dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Main
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <li className="py-1">
-                <Link to="/" className="dropdown-item">
-                  Home
-                </Link>
-              </li>
-              <li className="py-1">
-                <Link to="/test" className="dropdown-item">
-                  Test
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </li>
-        {/* user */}
-        <li>
-          <div className="dropdown ms-2">
-            <a className="text-decoration-none text-light dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              User
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <li className="py-1">
-                <Link to="/userList" className="dropdown-item">
-                  UserList
-                </Link>
-              </li>
-              <li className="py-1">
-                <Link to="/userLogin" className="dropdown-item">
-                  Login
-                </Link>
-              </li>
-              <li className="py-1">
-                <Link to="/userInsert" className="dropdown-item">
-                  Signup
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </li>
-        {/* board */}
-        <li>
-          <div className="dropdown ms-2">
-            <a className="text-decoration-none text-light dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Board
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <li className="py-1">
-                <Link to="/boardList" className="dropdown-item">
-                  BoardList
-                </Link>
-              </li>
-              <li className="py-1">
-                <Link to="/boardInsert" className="dropdown-item">
-                  BoardInsert
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </li>
-        {/* calendar */}
-        <li>
-          <div className="dropdown ms-2">
-            <a className="text-decoration-none text-light dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Calendar
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <li className="py-1">
-                <Link to="/calendarList" className="dropdown-item">
-                  CalendarList
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </li>
-        {/* food */}
-        <li>
-          <div className="dropdown ms-2">
-            <a className="text-decoration-none text-light dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Food
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <li className="py-1">
-                <Link to="/foodSearch" className="dropdown-item">
-                  FoodSearch
-                </Link>
-              </li>
-              <li className="py-1">
-                <Link to="/foodList" className="dropdown-item">
-                  FoodList
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </li>
-        {/* workout */}
-        <li>
-          <div className="dropdown ms-2">
-              <a className="text-decoration-none text-light dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Workout
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <li className="py-1">
-                  <Link to="/workoutList" className="nav-link fs-14 tc-w ms-2">
-                    WorkoutList
-                  </Link>
-                </li>
-                <li className="py-1">
-                  <Link to="/workoutInsert" className="nav-link fs-14 tc-w ms-2">
-                    WorkoutInsert
-                  </Link>
-                </li>
-              </ul>
-          </div>
-        </li>
-        {/* sleep */}
-        <li>
-          <div className="dropdown ms-2">
-            <a className="text-decoration-none text-light dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Sleep
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <li className="py-1">
-                <Link to="/sleepListDay" className="dropdown-item">
-                  SleepDay
-                </Link>
-              </li>
-              <li className="py-1">
-                <Link to="/sleepListWeek" className="dropdown-item">
-                  SleepWeek
-                </Link>
-              </li>
-              <li className="py-1">
-                <Link to="/sleepListMonth" className="dropdown-item">
-                  SleepMonth
-                </Link>
-              </li>
-              <li className="py-1">
-                <Link to="/sleepListYear" className="dropdown-item">
-                  SleepYear
-                </Link>
-              </li>
-              <li className="py-1">
-                <Link to="/sleepListSelect" className="dropdown-item">
-                  SleepSelect
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </li>
+      <ul className="nav bg-dark rounded">
+        {menus.map(menu => <DropdownMenu key={menu.label} {...menu} />)}
       </ul>
     );
   };
@@ -182,7 +107,6 @@ export const Header = () => {
         </Link>
       );
     };
-
     const buttonSignup = () => {
       return (
         <Link to="/userInsert">
@@ -192,7 +116,6 @@ export const Header = () => {
         </Link>
       );
     };
-
     if (!user_id || user_id === "false") {
       return (
         <form className="form-group d-center">
