e497fef8c45a765a3e08ee1720074451740a5e87
JUNGHO-GIT <junghomun00@gmail.com>
2024-05-24 05:54:30 +0900

    m

--- client/src/assets/js/handlerY.js
+++ client/src/assets/js/handlerY.js
@@ -8,29 +8,41 @@ export const handlerY = (
   let ticks = [];
   let maxValue = 0;
   let topValue = 0;
-
-  if (value && array) {
-    maxValue = Math.max(...value?.map((item) => Math.max(...array.map((key) => item[key]))));
+  let tickInterval = 0;
+  
+  if (type === "sleep") {
+    maxValue = Math.max(...value?.map((item) => (
+      Math.max(...array.map((key) => item[key]))
+    )));
+    topValue = 24;
+    tickInterval = 1;
+  }
+  else if (type === "money") {
+    maxValue = Math.max(...value?.map((item) => (
+      Math.max(...array.map((key) => item[key]))
+    )));
     topValue = Math.ceil(maxValue / 100) * 100;
-  }
-
-  // topValue에 따른 동적 틱 간격 설정
-  let tickInterval = 10;
-  if (topValue > 50) {
-    tickInterval = 50;
-  }
-  else if (topValue > 100) {
-    tickInterval = 100;
-  }
-  else if (topValue > 500) {
-    tickInterval = 500;
-  }
-  else if (topValue > 1000) {
     tickInterval = 1000;
   }
+  else if (type === "food") {
+    maxValue = Math.max(...value?.map((item) => (
+      Math.max(...array.map((key) => item[key]))
+    )));
+    topValue = Math.ceil(maxValue / 100) * 100;
+    tickInterval = 100
+  }
+  else if (type === "exercise") {
+    maxValue = Math.max(...value?.map((item) => (
+      Math.max(...array.map((key) => item[key]))
+    )));
+    topValue = Math.ceil(maxValue / 100) * 100;
+    tickInterval = 100
+  }
+
   for (let i = 0; i <= topValue; i += tickInterval) {
     ticks.push(i);
   }
+  
   return {
     domain: [0, topValue],
     ticks: ticks,
