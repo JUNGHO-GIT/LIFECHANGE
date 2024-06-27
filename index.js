647ef001b2df2f74a22bf138bc697ef4ec1c4ce5
JUNGHO-GIT <junghomun00@gmail.com>
2024-06-28 07:06:52 +0900

    'update'

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function startServer(httpPort, httpsPort) {
   // https 설정
   const options = {
     key: fs.readFileSync("key/privkey.pem"),
-    cert: fs.readFileSync("key/fullchain.pem"),
+    cert: fs.readFileSync("key/cert.pem"),
     ca: fs.readFileSync("key/chain.pem"),
   };
 
