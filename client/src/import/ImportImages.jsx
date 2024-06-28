cd4bafedf6343cb032f34025f5444f857420e168
JUNGHO-GIT <junghomun00@gmail.com>
2024-06-29 01:06:33 +0900

    'update'

--- client/src/import/ImportImages.jsx
+++ client/src/import/ImportImages.jsx
@@ -1,60 +1,60 @@
 // importImages.jsx
 // @ts-nocheck
 
-import logo1 from "../assets/images/logo/logo1.webp";
-import logo2 from "../assets/images/logo/logo2.webp";
-import logo3 from "../assets/images/logo/logo3.webp";
+import logo1 from "../assets/images/logo1.webp";
+import logo2 from "../assets/images/logo2.webp";
+import logo3 from "../assets/images/logo3.webp";
 
-import smile1 from "../assets/images/smile/smile1.webp";
-import smile2 from "../assets/images/smile/smile2.webp";
-import smile3 from "../assets/images/smile/smile3.webp";
-import smile4 from "../assets/images/smile/smile4.webp";
-import smile5 from "../assets/images/smile/smile5.webp";
+import smile1 from "../assets/images/smile1.webp";
+import smile2 from "../assets/images/smile2.webp";
+import smile3 from "../assets/images/smile3.webp";
+import smile4 from "../assets/images/smile4.webp";
+import smile5 from "../assets/images/smile5.webp";
 
-import flag1 from "../assets/images/flag/flag1.webp";
-import flag2 from "../assets/images/flag/flag2.webp";
+import flag1 from "../assets/images/flag1.webp";
+import flag2 from "../assets/images/flag2.webp";
 
-import common1 from "../assets/images/common/common1.webp";
-import common2 from "../assets/images/common/common2.webp";
-import common3_1 from "../assets/images/common/common3_1.webp";
-import common3_2 from "../assets/images/common/common3_2.webp";
-import common4 from "../assets/images/common/common4.webp";
-import common5 from "../assets/images/common/common5.webp";
+import common1 from "../assets/images/common1.webp";
+import common2 from "../assets/images/common2.webp";
+import common3_1 from "../assets/images/common3_1.webp";
+import common3_2 from "../assets/images/common3_2.webp";
+import common4 from "../assets/images/common4.webp";
+import common5 from "../assets/images/common5.webp";
 
-import calendar1 from "../assets/images/calendar/calendar1.webp";
-import calendar2 from "../assets/images/calendar/calendar2.webp";
-import calendar3 from "../assets/images/calendar/calendar3.webp";
+import calendar1 from "../assets/images/calendar1.webp";
+import calendar2 from "../assets/images/calendar2.webp";
+import calendar3 from "../assets/images/calendar3.webp";
 
-import exercise1 from "../assets/images/exercise/exercise1.webp";
-import exercise2 from "../assets/images/exercise/exercise2.webp";
-import exercise3_1 from "../assets/images/exercise/exercise3_1.webp";
-import exercise3_2 from "../assets/images/exercise/exercise3_2.webp";
-import exercise3_3 from "../assets/images/exercise/exercise3_3.webp";
-import exercise4 from "../assets/images/exercise/exercise4.webp";
-import exercise5 from "../assets/images/exercise/exercise5.webp";
+import exercise1 from "../assets/images/exercise1.webp";
+import exercise2 from "../assets/images/exercise2.webp";
+import exercise3_1 from "../assets/images/exercise3_1.webp";
+import exercise3_2 from "../assets/images/exercise3_2.webp";
+import exercise3_3 from "../assets/images/exercise3_3.webp";
+import exercise4 from "../assets/images/exercise4.webp";
+import exercise5 from "../assets/images/exercise5.webp";
 
-import food1 from "../assets/images/food/food1.webp";
-import food2 from "../assets/images/food/food2.webp";
-import food3 from "../assets/images/food/food3.webp";
-import food4 from "../assets/images/food/food4.webp";
-import food5 from "../assets/images/food/food5.webp";
+import food1 from "../assets/images/food1.webp";
+import food2 from "../assets/images/food2.webp";
+import food3 from "../assets/images/food3.webp";
+import food4 from "../assets/images/food4.webp";
+import food5 from "../assets/images/food5.webp";
 
-import money1 from "../assets/images/money/money1.webp";
-import money2 from "../assets/images/money/money2.webp";
-import money3 from "../assets/images/money/money3.webp";
-import money4 from "../assets/images/money/money4.webp";
+import money1 from "../assets/images/money1.webp";
+import money2 from "../assets/images/money2.webp";
+import money3 from "../assets/images/money3.webp";
+import money4 from "../assets/images/money4.webp";
 
-import sleep1 from "../assets/images/sleep/sleep1.webp";
-import sleep2 from "../assets/images/sleep/sleep2.webp";
-import sleep3 from "../assets/images/sleep/sleep3.webp";
-import sleep4 from "../assets/images/sleep/sleep4.webp";
+import sleep1 from "../assets/images/sleep1.webp";
+import sleep2 from "../assets/images/sleep2.webp";
+import sleep3 from "../assets/images/sleep3.webp";
+import sleep4 from "../assets/images/sleep4.webp";
 
-import user1 from "../assets/images/user/user1.webp";
+import user1 from "../assets/images/user1.webp";
 
-import setting1 from "../assets/images/setting/setting1.webp";
-import setting2 from "../assets/images/setting/setting2.webp";
-import setting3 from "../assets/images/setting/setting3.webp";
-import setting4 from "../assets/images/setting/setting4.webp";
+import setting1 from "../assets/images/setting1.webp";
+import setting2 from "../assets/images/setting2.webp";
+import setting3 from "../assets/images/setting3.webp";
+import setting4 from "../assets/images/setting4.webp";
 
 // -------------------------------------------------------------------------------------------------
 export {
