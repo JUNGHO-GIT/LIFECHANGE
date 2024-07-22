1f2920c0eeac3d797bc0b575340f4621294c65c6
JUNGHO-GIT <junghomun00@gmail.com>
2024-07-22 23:17:54 +0900

    update

--- gcloud.cjs
+++ gcloud.cjs
@@ -1,6 +1,32 @@
 // gcloud.cjs
 
+const dotenv = require('dotenv');
 const { execSync } = require('child_process');
+const { readFileSync, writeFileSync } = require('fs');
+
+// env 파일 수정하기
+const modifyEnv = () => {
+  const envFile = readFileSync('.env', 'utf8');
+  const envConfig = dotenv.parse(envFile);
+
+  // envConfig 수정
+  envConfig.CLIENT_URL = "https://www.junghomun.com";
+  envConfig.GOOGLE_CALLBACK_URL = "https://www.junghomun.com/api/google/callback";
+
+  // env 파일 쓰기
+  const newEnvFile = Object.keys(envConfig).reduce((acc, key) => {
+    return `${acc}${key}=${envConfig[key]}\n`;
+  });
+
+  writeFileSync('.env', newEnvFile);
+};
+
+// git push
+const gitPush = () => {
+  execSync('git add .', { stdio: 'inherit' });
+  execSync('git commit -m "update"', { stdio: 'inherit' });
+  execSync('git push origin master', { stdio: 'inherit' });
+}
 
 // run script on server
 const runRemoteScript = () => {
@@ -8,5 +34,25 @@ const runRemoteScript = () => {
   execSync(command, { stdio: 'inherit' });
 };
 
+// env 파일 복구하기
+const restoreEnv = () => {
+  const envFile = readFileSync('.env', 'utf8');
+  const envConfig = dotenv.parse(envFile);
+
+  // envConfig 수정
+  envConfig.CLIENT_URL = "http://localhost:3000";
+  envConfig.GOOGLE_CALLBACK_URL = "http://localhost:4000/api/google/callback";
+
+  // env 파일 쓰기
+  const newEnvFile = Object.keys(envConfig).reduce((acc, key) => {
+    return `${acc}${key}=${envConfig[key]}\n`;
+  });
+
+  writeFileSync('.env', newEnvFile);
+};
+
 // execute all steps
+modifyEnv();
+gitPush();
 runRemoteScript();
+restoreEnv();
