ba8d959bb289621c3044e1871fa55b9cdcc4d3c0
JUNGHO-GIT <junghomun00@gmail.com>
2024-08-06 22:01:36 +0900

    update

--- client/src/assets/hooks/useRoot.jsx
+++ client/src/assets/hooks/useRoot.jsx
@@ -1,37 +1,28 @@
 // useRoot.jsx
 
-import {useEffect, useNavigate, useLocation} from "../../import/ImportReacts.jsx";
+import { useEffect, useNavigate, useLocation } from "../../import/ImportReacts.jsx";
 
-// -------------------------------------------------------------------------------------------------
 export const useRoot = () => {
-
-  // 1. common -------------------------------------------------------------------------------------
   const navigate = useNavigate();
   const location = useLocation();
   const sessionId = sessionStorage.getItem('sessionId');
 
-  // 2-3. useEffect --------------------------------------------------------------------------------
   useEffect(() => {
+    // 로그인 페이지와 회원가입 페이지 접근 시, 세션이 존재하면 기본 페이지로 리다이렉트
+    if (sessionId && (
+      location.pathname === "/user/login" ||
+      location.pathname === "/user/signup"
+    )) {
+      navigate("/today/list");
+      return;
+    }
 
-    if (location.pathname !== "/user/login" && location.pathname !== "/user/signup") {
-
-      // '/'경로로 진입했을때의 처리
-      if (location.pathname === '/') {
-        if (!sessionId) {
-          navigate("/user/login");
-        }
-        else {
-          navigate("/today/list");
-        }
-      }
-
-      // 기타 다른 경로로 진입했을때의 처리
-      else {
-        if (!sessionId) {
-          navigate("/user/login");
-        }
-      }
+    // 로그인이 필요한 페이지에 세션이 없으면 로그인 페이지로 리다이렉트
+    if (!sessionId &&(
+      !location.pathname.startsWith("/user/login") &&
+      !location.pathname.startsWith("/user/signup")
+    )) {
+      navigate("/user/login");
     }
   }, [location, navigate, sessionId]);
-
 };
