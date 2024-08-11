a29e70bd35de4cb1f4777546c5943fd781459b54
JUNGHO-GIT <junghomun00@gmail.com>
2024-08-11 22:09:14 +0900

    update

--- gcloud.cjs
+++ gcloud.cjs
@@ -24,39 +24,6 @@ const modifyEnv = () => {
 };
 
 // -------------------------------------------------------------------------------------------------
-// git push
-const gitPush = () => {
-  execSync('git add .', { stdio: 'inherit' });
-  execSync('git commit -m "update"', { stdio: 'inherit' });
-  execSync('git push origin master', { stdio: 'inherit' });
-};
-
-// -------------------------------------------------------------------------------------------------
-// run script on server
-const runRemoteScript = () => {
-  const command = 'powershell -Command "ssh -i C:\\Users\\jungh\\.ssh\\JKEY junghomun00@34.23.233.23 \'sudo sh /sh/server.sh\'"';
-  execSync(command, { stdio: 'inherit' });
-};
-
-// -------------------------------------------------------------------------------------------------
-// env 파일 복구하기
-const restoreEnv = () => {
-  const envFile = readFileSync('.env', 'utf8');
-  const envConfig = dotenv.parse(envFile);
-
-  // envConfig 수정
-  envConfig.CLIENT_URL = "http://localhost:3000";
-  envConfig.GOOGLE_CALLBACK_URL = "http://localhost:4000/api/google/callback";
-
-  // env 파일 쓰기
-  const newEnvFile = Object.keys(envConfig).reduce((acc, key) => {
-    return `${acc}${key}=${envConfig[key]}${os.EOL}`;
-  }, '');
-
-  writeFileSync('.env', newEnvFile);
-};
-
-// -------------------------------------------------------------------------------------------------
 // modify changelog
 const modifyChangelog = () => {
 
@@ -90,8 +57,41 @@ const modifyChangelog = () => {
 };
 
 // -------------------------------------------------------------------------------------------------
+// git push
+const gitPush = () => {
+  execSync('git add .', { stdio: 'inherit' });
+  execSync('git commit -m "update"', { stdio: 'inherit' });
+  execSync('git push origin master', { stdio: 'inherit' });
+};
+
+// -------------------------------------------------------------------------------------------------
+// run script on server
+const runRemoteScript = () => {
+  const command = 'powershell -Command "ssh -i C:\\Users\\jungh\\.ssh\\JKEY junghomun00@34.23.233.23 \'sudo sh /sh/server.sh\'"';
+  execSync(command, { stdio: 'inherit' });
+};
+
+// -------------------------------------------------------------------------------------------------
+// env 파일 복구하기
+const restoreEnv = () => {
+  const envFile = readFileSync('.env', 'utf8');
+  const envConfig = dotenv.parse(envFile);
+
+  // envConfig 수정
+  envConfig.CLIENT_URL = "http://localhost:3000";
+  envConfig.GOOGLE_CALLBACK_URL = "http://localhost:4000/api/google/callback";
+
+  // env 파일 쓰기
+  const newEnvFile = Object.keys(envConfig).reduce((acc, key) => {
+    return `${acc}${key}=${envConfig[key]}${os.EOL}`;
+  }, '');
+
+  writeFileSync('.env', newEnvFile);
+};
+
+// -------------------------------------------------------------------------------------------------
 modifyEnv();
+modifyChangelog();
 gitPush();
 runRemoteScript();
 restoreEnv();
-modifyChangelog();
