9a8c42806a3e5acaaeac344df9f4fdd23e76da23
JUNGHO-GIT <junghomun00@gmail.com>
2024-10-07 02:23:20 +0900

    2024-10-07  2:23:20

--- client/src/components/Hr.tsx
+++ client/src/components/Hr.tsx
@@ -5,7 +5,7 @@ export const Hr = (props : any) => (
   <div
     className={props?.className}
     style={{
-      "background": "#dbdbdb",
+      "background": "#f2f2f2",
       "width": `${props?.w || 100}%`,
       "height": `${props?.h || 1}px`,
       "margin": `${props?.px/2}px 0px`,
