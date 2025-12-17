b53811fb3a6e3e135636cf0a6419947287cce6ee
JUNGHO-GIT <junghomun00@gmail.com>
2025-12-17 22:37:08 +0900

    2025-12-17 22:37:08

--- .node/mjs/gcloud.mjs
+++ .node/mjs/gcloud.mjs
@@ -152,8 +152,7 @@ const runServerRemoteScript = (pf = ``) => {
 		`sudo rm -rf client`,
 		`sudo chmod -R 755 ${serverPath}`,
 		`if pm2 describe ${env.projectName} >/dev/null 2>&1; then sudo pm2 stop ${env.projectName} && pm2 save; fi`,
-		`sudo rm -rf node_modules package-lock.json`,
-		`sudo npm install --legacy-peer-deps`,
+		`sudo ${args1} install --legacy-peer-deps`,
 		`sudo pm2 start ecosystem.config.cjs --env production && pm2 save`,
 		`sleep 5 && sudo pm2 save --force`,
 	].join(` && `);
