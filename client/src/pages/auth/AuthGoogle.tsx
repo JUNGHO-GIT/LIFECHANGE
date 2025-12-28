872a3e229c80c825348bea1eed288cf421007feb
JUNGHO-GIT <junghomun00@gmail.com>
2025-12-28 11:19:51 +0900

    2025-12-28 11:19:51

---
+++ client/src/pages/auth/AuthGoogle.tsx
@@ -0,0 +1,71 @@
+/**
+ * @file AuthGoogle.tsx
+ * @description foo
+ * @author Jungho
+ * @since 2025-12-26
+ */
+
+import { useEffect, memo } from "@exportReacts";
+import { useCommonValue } from "@exportHooks";
+import { axios } from "@exportLibs";
+import { useStoreAlert, useStoreLoading, useStoreLanguage } from "@exportStores";
+import { setLocal, setSession, sync } from "@exportScripts";
+
+// -------------------------------------------------------------------------------------------------
+export const AuthGoogle = memo(() => {
+	// 1. common -------------------------------------------------------------------------------------
+	const {
+		URL_GOOGLE, navigate, toCalendarList,
+	} = useCommonValue();
+	const { translate } = useStoreLanguage();
+	const { setALERT } = useStoreAlert();
+	const { setLOADING } = useStoreLoading();
+
+	// 2-3. useEffect --------------------------------------------------------------------------------
+	useEffect(() => {
+		axios
+		.get(`${URL_GOOGLE}/afterCallback`)
+		.then((res: unknown) => {
+			if (res.data.status === `success`) {
+				// localStorage
+				setLocal(`setting`, `id`, ``, {
+					autoLogin: `true`,
+					autoLoginId: res.data.googleId,
+					autoLoginPw: res.data.googlePw,
+					isGoogle: `true`,
+				});
+
+				// sessionStorage
+				setSession(`setting`, `id`, ``, {
+					sessionId: res.data.googleId,
+					admin: res.data.admin === `admin` ? `true` : `false`,
+				});
+				void navigate(toCalendarList);
+				void sync();
+			}
+			else {
+				setLOADING(false);
+				setALERT({
+					open: true,
+					msg: translate(res.data.msg as string),
+					severity: `error`,
+				});
+			}
+		})
+		.catch((error: unknown) => {
+			setLOADING(false);
+			setALERT({
+				open: true,
+				msg: translate(error as string),
+				severity: `error`,
+			});
+			console.error(error);
+		})
+		.finally(() => {
+			setLOADING(false);
+		});
+	}, []);
+
+	// 10. return ------------------------------------------------------------------------------------
+	return <></>;
+});
---
+++ client/src/schemas/admin/Admin.tsx
@@ -0,0 +1,26 @@
+/**
+ * @file Admin.tsx
+ * @description foo
+ * @author Jungho
+ * @since 2025-12-26
+ */
+
+// Types ------------------------------------------------------------------------------------------
+export type AppInfoType = {
+	_id: string;
+	admin_name: string;
+	admin_email: string;
+	admin_role: string;
+	admin_regDt: string;
+	admin_updateDt: string;
+};
+
+// Schema -----------------------------------------------------------------------------------------
+export const AppInfo: AppInfoType = {
+	_id: ``,
+	admin_name: ``,
+	admin_email: ``,
+	admin_role: `user`,
+	admin_regDt: ``,
+	admin_updateDt: ``,
+};
